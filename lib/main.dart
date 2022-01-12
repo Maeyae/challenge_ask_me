7ada8ea01b6be2debffec8c4a8f22638d04f57ef
maeva <maeganelys@gmail.com>
2022-01-12 09:13:52 +0400

    after adding 1 ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Myapp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+
+
+class Myapp extends StatefulWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  _MyappState createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-   home: Scaffold(
-     backgroundColor: Colors.blue,
-     appBar: AppBar(
-       title: Center(child: Text('Ask Me Anything')),
-       backgroundColor: Colors.lightBlue,
-     ),
-   ),
+      home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Center(child: Text('Ask Me Anything')),
+            backgroundColor: Colors.lightBlue,
+          ),
+          body:Center(
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          print('Clique');
+                        },
+                        child: Image.asset('images/ball1.png'))),
+
+              ],
+            ),
+          )
+      ),
     );
   }
 }
